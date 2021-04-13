bd5120b17875d0f64b7b171e565d54e3f40cd26f
vermad843 <vishnuverma7524@gmail.com>
2021-04-13 20:52:59 +0530

    airbnb logo clickable

--- src/Header.js
+++ src/Header.js
@@ -4,15 +4,18 @@ import LanguageIcon from '@material-ui/icons/Language';
 import SearchIcon from '@material-ui/icons/Search';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import Avatar from '@material-ui/core/Avatar';
+import {Link} from 'react-router-dom';
 
 function Header() {
     return (
         <div className = "header">
-            <img
-                className = "header__icon"
-                src = "https://logodownload.org/wp-content/uploads/2016/10/airbnb-logo-10.png"
-                alt = ""
-            />
+           <Link to = "/">
+               <img
+                    className = "header__icon"
+                    src = "https://logodownload.org/wp-content/uploads/2016/10/airbnb-logo-10.png"
+                    alt = ""
+                />
+           </Link> 
             <div className = "header__center">
                <input
                    type = "text"

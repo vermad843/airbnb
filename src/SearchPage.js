5117ca06423eb438bd5b21a0ddb583bd08910a86
vermad843 <vishnuverma7524@gmail.com>
2021-04-13 16:20:35 +0530

    SearchPage

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,10 +1,44 @@
 import React from 'react';
 import './SearchPage.css';
+import Button from '@material-ui/core/Button';
 
 function SearchPage() {
     return (
         <div className = "searchPage">
-            <h1>This is a SearchPage</h1>
+            <div className = "searchPage__info">
+                <p>
+                    62 stays . 26 august to 30
+                    august . guest
+                </p>
+                <h1>
+                    Stays nearby
+                </h1>
+                <Button
+                   variant = "outlined"
+                   >
+                    Cancellation Flexibility
+                </Button>
+                <Button
+                   variant = "outlined"
+                   >
+                    Type of place
+                </Button>
+                <Button
+                   variant = "outlined"
+                   >
+                    Price
+                </Button>
+                <Button
+                   variant = "outlined"
+                   >
+                    Rooms and beds
+                </Button>
+                <Button
+                   variant = "outlined"
+                   >
+                    More filters
+                </Button>
+            </div>
         </div>
     )
 }
